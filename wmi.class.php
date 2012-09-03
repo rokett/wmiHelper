f45c8ee7df0b8a4814e6432e9ecdab1c41e7daa9
Adam Nickells <adamnickells@gmail.com>
2012-09-04 00:18:02 +0100

    Updating class parameter comments

--- wmi.class.php
+++ wmi.class.php
@@ -7,8 +7,10 @@ class wmi {
 	/**
 	 * Create a new wmi instance.
 	 *
-	 * @param  string  $host
-	 * @return void
+	 * @param	string	$host		Host name or IP address to connect to
+	 * @param	string	$username	Local host user with rights to query WMI; normally a local admin
+	 * @param	string	$password	Password of local user account
+	 * @return	void				New wmi object
 	 */
 	public function __construct($host = null, $username = null, $password = null) {
 		$wmiLocator = new COM('WbemScripting.SWbemLocator');
@@ -19,8 +21,8 @@ class wmi {
 	/**
 	 * Get all properties of a WMI class.
 	 *
-	 * @param string   $win32_class
-	 * @return object
+	 * @param	string	$win32_class	Win32 class to retrieve data from
+	 * @return	object					WMI collection object
 	 */
 	public function getInfo($win32_class) {
 		$WMIcollection = $this->connection->ExecQuery('SELECT * FROM ' . $win32_class);
